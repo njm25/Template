834e373d0298824a96340b4cc634920dd2b42835
nicomilette <98992077+nicomilette@users.noreply.github.com>
2025-11-19 21:00:49 -0500

    chore: more cors configuration

--- client/src/services/apiHandler/apiHandler.ts
+++ client/src/services/apiHandler/apiHandler.ts
@@ -3,6 +3,11 @@ import { toast } from "react-toastify";
 
 const apiHandler = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, 
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
 });
 
 apiHandler.interceptors.response.use(
