8361f48986145e738ff5e9b7d73c400e074000c4
njm25 <nicomilette7@gmail.com>
2025-12-14 13:39:47 -0500

    feat: add different secrets

--- server/src/services/aboutService.ts
+++ server/src/services/aboutService.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
 import { BaseService } from "../core/baseService.ts";
 
+const secrets = [
+	"Shhh super secret!!!",
+	"Ahhh now thats a secret!!!",
+	"Ooooooh dont tell anyone!!!",
+	"This is a mystic secret!!!",
+	"Wowza balowza secret!!!",
+	"I know a secret!!!",
+	"Shhhh dont tell anyone!!!",
+];
+
 export class AboutService extends BaseService {
 	protected registerRoutes(): void {
 		this.app.get("/about/error", (req: Request, res: Response) => {
@@ -8,7 +18,7 @@ export class AboutService extends BaseService {
 		});
 
 		this.app.get("/about/secret", (req: Request, res: Response) => {
-			res.status(200).send("This is a secret!!!!");
+			res.status(200).send(secrets[Math.floor(Math.random() * secrets.length)]);
 		});
 	}
 }
